587b8b5aa1f462ea6a7d39e0112bd2a87b5ffc6a
akshi <akshi20jain03@gmail.com>
2026-05-14 01:52:35 +0530

    ADDED: authenctication using clerk

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,25 @@
+import { ClerkProvider } from "@clerk/expo";
+import { tokenCache } from '@clerk/expo/token-cache';
 import { DefaultTheme, ThemeProvider } from "@react-navigation/native";
 import { Slot } from "expo-router";
 import { View } from "react-native";
 import "../global.css";
 
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
+
+if (!publishableKey) {
+  throw new Error("EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY is not defined");
+}
+
 export default function RootLayout() {
   return (
-    <ThemeProvider value={DefaultTheme}>
-      <View style={{ flex: 1, backgroundColor: 'white' }}>
-        <Slot />
-      </View>
-    </ThemeProvider>
+    <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
+      <ThemeProvider value={DefaultTheme}>
+        <View style={{ flex: 1, backgroundColor: 'white' }}>
+          <Slot />
+        </View>
+      </ThemeProvider>
+    </ClerkProvider>
 
   );
 }
